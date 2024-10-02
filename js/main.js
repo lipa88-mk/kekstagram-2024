570d40d3e3fd27bcc752a017db60decc32141b25
maria.korshunova <maria.korshunova@solveva.com>
2024-10-02 18:40:12 +0200

    refactor getRandomPositiveInteger function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  //Максимум и минимум включаются
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
-getRandomInt(1, 10);
+getRandomPositiveInteger(1, 10);
 
 const isMaxLengthFit = (str, maxLength) => (str.length <= maxLength);
 isMaxLengthFit('qwe', 5);
